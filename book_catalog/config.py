7466ede4dc4e1d07e600dbbd38a76a31fcb22222
Roomnumber13 <rajkumar.rajendran197@gmail.com>
2024-09-01 14:53:34 +1000

    Update file

--- book_catalog/config.py
+++ book_catalog/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Settings:
-    DATABASE_URL: str = os.getenv("DATABASE_URL")
+    DATABASE_URL: str = os.getenv("postgresql://sit722_part2_zdo7_user:xyOUKEjlrs0JAYdf9BqZDrAIg1ggmPZI@dpg-cr7h22i3esus7389oj50-a.singapore-postgres.render.com/sit722_part2_zdo7")
 
 settings = Settings()
